0da4c2709b5207dae32183919b4061121901c6a9
Kai Feng (Mini) <kaifeng_toh@mymail.sutd.edu.sg>
2022-12-27 19:25:14 +0800

    migrated to react

--- src/app.tsx
+++ src/app.tsx
@@ -1,24 +1,24 @@
-import { useState } from "preact/hooks";
-import "./app.css";
+import React from "react";
+import "./App.css";
 import Biography from "./layout/biography";
 
-export function App() {
+export default function App() {
   return (
-    <div class="landing-page">
+    <div className="landing-page">
       <div className="menubar">
         <div className="menubar-title">
           <div className="blue">TOH&nbsp;</div>
           KAI FENG
         </div>
       </div>
-      <div class="cover">
+      <div className="cover">
         <div className="profile-pic">
           <img src="/profilepic.jpeg" />
         </div>
-        <div class="name-box">
-          <div class="name-top">Toh</div>
-          <div class="name-bottom">KaiFeng</div>
-          <div class="profile-title">FULL STACK SOFTWARE ENGINEER</div>
+        <div className="name-box">
+          <div className="name-top">Toh</div>
+          <div className="name-bottom">KaiFeng</div>
+          <div className="profile-title">FULL STACK SOFTWARE ENGINEER</div>
         </div>
       </div>
       <Biography />
