0789291b1f27fb8fde69b36991039bbee2e38dda
Kai Feng (Air) <kaifeng_toh@mymail.sutd.edu.sg>
2023-01-04 14:01:51 +0800

    started thumbnail

--- src/layout/SingleProject.tsx
+++ src/layout/SingleProject.tsx
@@ -6,7 +6,8 @@ import { SwiperSlide, Swiper } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/pagination';
 
-import { Pagination } from 'swiper';
+import { Navigation, Pagination, Thumbs } from 'swiper';
+import { useState } from 'react';
 
 export const getProject = async ({ params }: any) => {
   const response = await fetch(`${import.meta.env.BASE_URL}/projects/${params.id}.json`);
@@ -15,8 +16,7 @@ export const getProject = async ({ params }: any) => {
 };
 
 export default function SingleProject() {
-  // get project id from react router
-  const { id } = useParams<{ id: string }>();
+  const [activeThumb, setActiveThumb] = useState<any | null>();
 
   // get project from database
   const { project }: any = useLoaderData();
@@ -28,15 +28,14 @@ export default function SingleProject() {
       <div className="divider" />
       <div className="project-gallery">
         <Swiper
-          pagination={{
-            el: '.my-custom-pagination-div',
-            dynamicBullets: true,
-            clickable: true,
-            renderBullet: (index, className) => {
-              return '<span class="' + className + '">' + (index + 1) + '</span>';
-            },
+          spaceBetween={10}
+          navigation={true}
+          modules={[Navigation, Thumbs]}
+          grabCursor={true}
+          thumbs={{
+            swiper: activeThumb && !activeThumb.destroyed ? activeThumb : null,
           }}
-          modules={[Pagination]}
+          className="product-images-slider"
         >
           {project.gallery.map((project: any, index: number) => (
             <SwiperSlide key={index}>
@@ -44,8 +43,25 @@ export default function SingleProject() {
             </SwiperSlide>
           ))}
         </Swiper>
-        <div className="my-custom-pagination-div" />
       </div>
+      <Swiper
+        onSwiper={(swiper) => {
+          console.log('swiper:', swiper);
+          setActiveThumb(swiper);
+        }}
+        spaceBetween={10}
+        slidesPerView={4}
+        modules={[Navigation, Thumbs]}
+        className="product-images-slider-thumbs"
+      >
+        {project.gallery.map((project: any, index: number) => (
+          <SwiperSlide key={index}>
+            <div className="product-images-slider-thumbs-wrapper">
+              <img src={import.meta.env.BASE_URL + `${project.image}`} key={index} />
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
       <div className="project-description">{project.description}</div>
     </div>
   );
